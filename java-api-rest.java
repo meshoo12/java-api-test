a773719e081ff91e61c1690c45695dc6eb8a255b
Ahmed <meshoo12@users.noreply.github.com>
2021-08-11 20:59:11 +0200

    Create java-api-rest.java

---
+++ java-api-rest.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Base64;
+
+
+
+public class App {
+
+public static String username = "Test_Username";
+
+    public static void main(String[] args) {
+        
+
+        try {
+            URL url = new URL ("http://api.com/" + username);
+            String encoding = Base64.getEncoder().encodeToString(":".getBytes("utf-8"));
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setDoOutput(true);
+            connection.setRequestProperty  ("Authorization", "Basic " + encoding);
+            InputStream content = (InputStream)connection.getInputStream();
+            BufferedReader in   = 
+                new BufferedReader (new InputStreamReader (content));   
+         String line = "";
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch(Exception e) {
+           e.printStackTrace();
+        }
+    }
+}
